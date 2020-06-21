5794340e7cacafefd3b96b91a383c32f39610f12
花田　剛章 <takaaki.hanada@neuromagic.com>
2020-06-21 15:33:55 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,70 @@
+import axios from 'axios'
+
+export default {
+  
+  target: 'static',
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
+  build: {
+  },
+
+  generate: {
+    async routes() {
+      const pages = await axios
+        .get('https://banashade08.microcms.io/api/v1/blog?limit=100', {//100件の記事を取得
+          headers: { 'X-API-KEY': '28fa12c1-cc1d-4bd0-af75-c142099c45e4' }
+        })
+        .then((res) =>
+          res.data.contents.map((content) => ({
+            route: `/${content.id}`,
+            payload: content
+          }))
+        )
+      return pages
+    }
+  }
+
+}
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://banashade08.microcms.io/api/v1/blog',
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-API-KEY': '28fa12c1-cc1d-4bd0-af75-c142099c45e4' }
+      }
+    )
+    return data
+  }
+}
+</script>
